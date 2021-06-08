e88b1de8038b4db97d77aa59cfabc84ddf78c767
Mikel Sanabria <mikel.sanabria@gmail.com>
2021-06-09 01:10:17 +0200

    POST /customers end-point #27

--- src/app/workshop/customers/customer-dialog/customer-creation-update.model.ts
+++ src/app/workshop/customers/customer-dialog/customer-creation-update.model.ts
@@ -1,11 +1,10 @@
 export interface CustomerCreationUpdate {
   id?: string;
   identificationId: string;
-  completeName?: string;
+//  completeName?: string;
   name: string;
   surName: string;
   secondSurName: string;
-//  registrationDate: Date; BACKEND
   lastVisitDate?: string;
   phone: string;
   mobilePhone: string;
