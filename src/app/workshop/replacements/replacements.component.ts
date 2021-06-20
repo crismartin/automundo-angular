54c0d3c58f9ac69bb321e802c135a8895eb97758
Cristian Fabian Martinez <cris.mtxz@gmail.com>
2021-06-20 03:52:28 +0200

    Update Cost in Revision UI. #73

--- src/app/workshop/replacements/replacements.component.ts
+++ src/app/workshop/replacements/replacements.component.ts
@@ -27,7 +27,7 @@ export class ReplacementsComponent {
   dataSource: MatTableDataSource<ReplacementUsed>;
   nameReplacementSelectForm = '';
   replacementsOptions: Replacement[];
-
+  totalCost: string;
 
   constructor(private snackBar: MatSnackBar, private replacementsService: ReplacementsService,
               private sharedReplacementService: SharedReplacementService, private revisionService: RevisionService) {
@@ -144,7 +144,9 @@ export class ReplacementsComponent {
   }
 
   changeReplacementUsedForm(event: MatSelectChange): void {
+    console.log(event);
     this.nameReplacementSelectForm = event.source.triggerValue;
+    this.setFinalPriceReplacement();
   }
 
   setTableAndRefresh(replacementsUsed: ReplacementUsed[]): void{
@@ -154,6 +156,17 @@ export class ReplacementsComponent {
 
   refreshTable(): void {
     this.dataSource = new MatTableDataSource<ReplacementUsed>(this.replacementsUsed);
+    this.updateTotalCost();
+  }
+
+  updateTotalCost(): void {
+    const cero = 0.00;
+    let result: number = cero;
+    if (this.replacementsUsed.length > 0){
+      result = this.replacementsUsed.map(replacement => replacement.price === undefined ? 0.00 : replacement.price)
+        .reduce((previousValue, currentValue) => previousValue + currentValue);
+    }
+    this.totalCost = result.toFixed(2);
   }
 
   serializeForm(formData: any): ReplacementUsed {
@@ -161,20 +174,47 @@ export class ReplacementsComponent {
       reference: formData.reference,
       quantity: formData.quantity,
       own: formData.own,
-      price: formData.price,
-      discount: formData.discount,
+      price: parseFloat(formData.price),
+      discount: parseInt(formData.discount, 0),
       replacement: {
-        reference: formData.replacement
+        reference: formData.replacement,
+        name: this.getNameReplacement(formData.replacement)
       },
       revisionReference: formData.revisionReference
     };
+  }
+
+  setFinalPriceReplacement(): void {
+    let resultPrice = '0.00';
+    const quantity = toInteger(this.replacementUsedForm.value.quantity);
+    const replacementReference = this.replacementUsedForm.value.replacement;
+    const discountPercent = toInteger(this.replacementUsedForm.value.discount) / 100;
+
+    const replacement = this.replacementsOptions.filter(replacementOption => replacementOption.reference === replacementReference)[0];
+    if (replacement !== undefined && quantity > 0){
+      console.log(this.replacementUsedForm.value);
+      resultPrice = (replacement.price * quantity * (1 - discountPercent)).toFixed(2);
+      this.replacementUsedForm.patchValue({price: resultPrice});
+    }else{
+      console.log(this.replacementUsedForm.value);
+    }
+  }
+
+  getNameReplacement(reference: string): string {
+    let result = '';
+    if (this.replacementsOptions.length > 0){
+      const replacement = this.replacementsOptions.filter(value => value.reference === reference)[0];
+      result = replacement.name;
+    }
+
+    return result;
   }
 }
 
 function templateForm(replacementUsed: ReplacementUsed): FormGroup {
   return new FormGroup({
     reference: new FormControl(replacementUsed.reference),
-    quantity: new FormControl(replacementUsed.quantity, [Validators.maxLength(3), Validators.pattern('[0-9]+')]),
+    quantity: new FormControl(replacementUsed.quantity, [Validators.required, Validators.maxLength(3), Validators.pattern('[0-9]+')]),
     own: new FormControl(replacementUsed.own),
     price: new FormControl(replacementUsed.price, [Validators.maxLength(10), Validators.pattern('^[1-9]\\d*(\\.\\d+)?$')]),
     discount: new FormControl(replacementUsed.discount, [Validators.maxLength(3), Validators.pattern('[0-9]+')]),
@@ -184,10 +224,29 @@ function templateForm(replacementUsed: ReplacementUsed): FormGroup {
 }
 
 function hasRevision(replacementUsed: ReplacementUsed): boolean {
-  return replacementUsed.revisionReference !== undefined;
+  const nullables = [null, undefined];
+  return nullables.indexOf(replacementUsed.revisionReference) === -1;
 }
 
 function hasReference(replacementUsed: ReplacementUsed): boolean {
   const nullables = ['', null, undefined];
   return nullables.indexOf(replacementUsed.reference) === -1;
+}
+
+function toInteger(numberStr: string): number {
+  let result = 0;
+  const nullables = ['', null, undefined];
+  if (nullables.indexOf(numberStr) < 0){
+    result = parseInt(numberStr, 0);
+  }
+  return result;
+}
+
+function toFloat(numberStr: string): number {
+  let result = 0;
+  const nullables = ['', null, undefined];
+  if (nullables.indexOf(numberStr) < 0){
+    result = parseFloat(numberStr);
+  }
+  return result;
 }
